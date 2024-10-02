7aa2a0b3f1de54ef7bcd67c16e866a51c19316df
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-10-02 13:55:50 +0200

    Implement armor set capes

---
+++ src/main/java/sct/hexxitgear/render/RenderCape.java
@@ -0,0 +1,46 @@
+package sct.hexxitgear.render;
+
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import sct.hexxitgear.HexxitGear;
+import sct.hexxitgear.core.ArmorSet;
+
+import java.util.UUID;
+
+@Mod.EventBusSubscriber(modid = HexxitGear.MODID, value = Side.CLIENT)
+public class RenderCape {
+
+    private static final ResourceLocation SAGE_CAPE_TEXTURE = new ResourceLocation(HexxitGear.MODID, "textures/armor/capes/sage.png");
+    private static final ResourceLocation SCALE_CAPE_TEXTURE = new ResourceLocation(HexxitGear.MODID, "textures/armor/capes/scale.png");
+    private static final ResourceLocation THIEF_CAPE_TEXTURE = new ResourceLocation(HexxitGear.MODID, "textures/armor/capes/thief.png");
+    private static final ResourceLocation TRIBAL_CAPE_TEXTURE = new ResourceLocation(HexxitGear.MODID, "textures/armor/capes/tribal.png");
+    private static final ResourceLocation ANCIENT_THIEF_CAPE_TEXTURE = new ResourceLocation(HexxitGear.MODID, "textures/armor/capes/ancient_thief.png");
+    private static final ResourceLocation ANCIENT_TRIBAL_CAPE_TEXTURE = new ResourceLocation(HexxitGear.MODID, "textures/armor/capes/ancient_tribal.png");
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void onRenderPlayer(RenderPlayerEvent.Post event) {
+        NetHandlerPlayClient connection = Minecraft.getMinecraft().getConnection();
+        if (connection != null) {
+            EntityPlayer player = event.getEntityPlayer();
+            UUID playerId = player.getUniqueID();
+            ArmorSet set = ArmorSet.getCurrentArmorSet(player);
+            connection.getPlayerInfo(playerId).playerTextures.put(MinecraftProfileTexture.Type.CAPE, getCapeTexture(set));
+        }
+    }
+
+    private static ResourceLocation getCapeTexture(ArmorSet set) {
+        if (set == ArmorSet.SAGE) return SAGE_CAPE_TEXTURE;
+        if (set == ArmorSet.SCALE) return SCALE_CAPE_TEXTURE;
+        if (set == ArmorSet.THIEF) return THIEF_CAPE_TEXTURE;
+        if (set == ArmorSet.TRIBAL) return TRIBAL_CAPE_TEXTURE;
+        return null;
+    }
+}
