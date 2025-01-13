ef4bbdcef584b21e7e30305d2a4aa918c4f2f6b4
ACGaming <4818419+ACGaming@users.noreply.github.com>
2025-01-13 09:53:08 +0100

    Optimize master sword rendering

--- src/main/java/sct/hexxitgear/render/RenderMiniSword.java
+++ src/main/java/sct/hexxitgear/render/RenderMiniSword.java
@@ -1,7 +1,5 @@
 package sct.hexxitgear.render;
 
-import javax.annotation.Nonnull;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderItem;
@@ -15,50 +13,50 @@ import net.minecraft.util.math.MathHelper;
 import sct.hexxitgear.entity.EntityMiniSword;
 import sct.hexxitgear.init.HexRegistry;
 
+import javax.annotation.Nonnull;
+
 public class RenderMiniSword extends Render<EntityMiniSword> {
+    private static final ItemStack PROJECTILE = new ItemStack(HexRegistry.HEXICAL_MASTER_SWORD_ACTIVATION);
+
     public RenderMiniSword(RenderManager renderManager) {
         super(renderManager);
     }
 
     @Override
-    public void doRender(@Nonnull EntityMiniSword entity, double d, double d1, double d2, float f, float f1) {
+    public void doRender(@Nonnull EntityMiniSword entity, double x, double y, double z, float yaw, float partialTicks) {
         RenderItem itemRender = Minecraft.getMinecraft().getRenderItem();
         GlStateManager.pushMatrix();
         bindEntityTexture(entity);
-        GlStateManager.translate(d, d1, d2);
+        GlStateManager.translate(x, y, z);
         GlStateManager.enableRescaleNormal();
         GlStateManager.scale(0.85f, 0.85f, 0.85f);
-        GlStateManager.rotate(entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * f1 - 90.0f, 0.0f, 1.0f, 0.0f);
-        GlStateManager.rotate(entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * f1 - 45.0f, 0.0f, 0.0f, 1.0f);
-        this.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-        float f15 = entity.arrowShake - f1;
-        
-        if (f15 > 0.0f) {
-            float f16 = -MathHelper.sin(f15 * 3.0f) * f15;
-            GlStateManager.rotate(f16, 0.0f, 0.0f, 1.0f);
+        GlStateManager.rotate(entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * partialTicks - 90.0f, 0.0f, 1.0f, 0.0f);
+        GlStateManager.rotate(entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks - 45.0f, 0.0f, 0.0f, 1.0f);
+        bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+        float f1 = entity.arrowShake - partialTicks;
+
+        if (f1 > 0.0f) {
+            float f2 = -MathHelper.sin(f1 * 3.0f) * f1;
+            GlStateManager.rotate(f2, 0.0f, 0.0f, 1.0f);
         }
-        
+
         GlStateManager.translate(-0.15f, -0.15f, 0.0f);
-        
+
         if (renderOutlines) {
             GlStateManager.enableColorMaterial();
             GlStateManager.enableOutlineMode(8214271);
         }
-        
-        itemRender.renderItem(getStackToRender(entity), TransformType.NONE);
-        
+
+        itemRender.renderItem(PROJECTILE, TransformType.NONE);
+
         if (renderOutlines) {
             GlStateManager.disableOutlineMode();
             GlStateManager.disableColorMaterial();
         }
-        
+
         GlStateManager.disableRescaleNormal();
         GlStateManager.popMatrix();
-        super.doRender(entity, d, d1, d2, f, f1);
-    }
-
-    public ItemStack getStackToRender(EntityMiniSword entity) {
-        return new ItemStack(HexRegistry.HEXICAL_MASTER_SWORD_ACTIVATION);
+        super.doRender(entity, x, y, z, yaw, partialTicks);
     }
 
     @Override
